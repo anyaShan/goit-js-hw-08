c0e43745aa6cc70d1392a578317cd2c833dfd896
anya.shan <anya.shanaida@yahoo.com>
2022-08-08 15:59:37 +0300

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
+
+const listGalleryEl = document.querySelector('.gallery');
+
+const markupGalleryEl = markupItemsGalleryEl(galleryItems);
+listGalleryEl.insertAdjacentHTML('beforeend', markupGalleryEl);
+
+function markupItemsGalleryEl(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                />
+            </a>
+        </li>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 // Change code below this line
 
 console.log(galleryItems);
